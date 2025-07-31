50484dcdd16676621b708f70e6915db5f29a7f36
IlYAN-FISHERMAN <Ilkay.yanar71@proton.me>
2025-07-31 14:23:13 +0200

    ioMap finish

--- IoMonitor/tests/testMain.cc
+++ IoMonitor/tests/testMain.cc
@@ -1,21 +1,31 @@
 #include <gtest/gtest.h>
 #include "tester.hh"
 
-// TEST(IoStat, CleanData) {
-// 	EXPECT_EQ(testIoStatCleaning(), 0);
+TEST(IoStat, CleanData) {
+	EXPECT_EQ(testIoStatCleaning(), 0);
+}
+
+TEST(IoStat, FillData) {
+	EXPECT_EQ(testIoStatFillData(), 0);
+}
+
+TEST(IoMap, FillData) {
+	EXPECT_EQ(testIoMap(), 0);
+}
+
+/// Only for debugging
+// TEST(IoMap, testWithInteraction) {
+// 	EXPECT_EQ(testInteractiveIoMap(), 0);
 // }
-//
-// TEST(IoStat, FillData) {
-// 	EXPECT_EQ(testIoStatFillData(), 0);
-// }
-//
+
 int main(int ac, char **av) {
 	(void)ac;
 	(void)av;
 
 	// testIoMap();
-	testInteractiveIoMap();
+	// testInteractiveIoMap();
 	// testIoStatFillData();
+	// testIoStatCleaning();
     // ::testing::InitGoogleTest(&ac, av);
     // return RUN_ALL_TESTS();
 }
